e5ab8611747a7a4ef363944723c9f91cd0833763
Conor <conbrocox@gmail.com>
2016-11-30 19:32:09 -0700

    It's perfect now

--- JavaPrinter/src/visitor/Commenter.java
+++ JavaPrinter/src/visitor/Commenter.java
@@ -1,6 +1,7 @@
 // Conor Cox
 // CS414
 // A6
+// Commenter.java
 
 package visitor;
 
@@ -11,7 +12,7 @@ public class Commenter extends TreeDumper {
 	
 	@Override
 	public void visit(MethodDeclaration n) {
-		System.out.println("/*************");
+		System.out.println("\n/*************");
 		System.out.println("New method " + n.f2.f0);
 		System.out.println("*************/");
 		this.startAtNextToken();
@@ -20,7 +21,6 @@ public class Commenter extends TreeDumper {
         n.f2.accept(this);
         n.f3.accept(this);  
         n.f4.accept(this);
-        System.out.println();
 	}
 	
 	@Override
@@ -32,12 +32,11 @@ public class Commenter extends TreeDumper {
         n.f0.accept(this);
         n.f1.accept(this);
 
-        System.out.println();
 	}
 	
 	@Override
 	public void visit(ConstructorDeclaration n) {
-		System.out.println("/*************");
+		System.out.println("\n/*************");
 		System.out.println("New constructor " + n.f1);
 		System.out.println("*************/");
 		this.startAtNextToken();
@@ -49,37 +48,28 @@ public class Commenter extends TreeDumper {
         n.f5.accept(this);
         n.f6.accept(this);  
         n.f7.accept(this);
-        System.out.println();
 	}
 	
 	@Override
 	public void visit(NestedClassDeclaration n) {
-		System.out.println("/*************");
+		System.out.println("\n/*************");
 		System.out.println("New nested class " + n.f1.f1);
 		System.out.println("*************/");
 		this.startAtNextToken();
         n.f0.accept(this);
         n.f1.accept(this);
-        System.out.println();
 	}
 	
 	@Override
 	public void visit(FieldDeclaration n) {
-		// This is needed  for if the variable is the very first thing within a method
-		// I realize that this is needed for every visit method... Will ask if spacing matters all the much in class
-		
-		if(stupidLine == 0){
-			System.out.println();
-			stupidLine++;
-		}
-		System.out.println("// Class variable definition begins");
+		System.out.println("\n// Class variable definition begins");
 		this.startAtNextToken();
 		n.f0.accept(this);
         n.f1.accept(this);
         n.f2.accept(this);
         n.f3.accept(this);  
         n.f4.accept(this);
-        System.out.println("\n// Class variable definition ends");
+        System.out.print("\n// Class variable definition ends");
 	}
 	
 }
